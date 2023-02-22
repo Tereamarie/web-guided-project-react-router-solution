be14d17b1cbada3df0eb9c01f529b1903233b3d4
Gabriel Cabrejas <gabriel@cabrejas.net>
2023-02-22 15:59:21 +0100

    rr6

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Importing the top-level component
-import App from './components/App'
-
-// Importing the styles
-import './styles.less'
-
-// 👉 STEP 1 - Import React Router's Router
-/*
-  INSTRUCTOR: you could explain there's a hashRouter that uses the URL hash for routing
-*/
-import { BrowserRouter as Router } from 'react-router-dom'
-
-render(
-  <Router>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </Router>
-  , document.querySelector('#root')
-)
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
